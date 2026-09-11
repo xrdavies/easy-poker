ba07775e6b8304ff45e3b806d9ea3bb9dae1455f
Frozen <1884084+xrdavies@users.noreply.github.com>
2026-09-12 07:35:01 +0800

    fix: fix the api url replacement

--- public/js/app.js
+++ public/js/app.js
@@ -140,7 +140,9 @@ function chipStackHTML(amount) {
 function inferApiOrigin() {
   const host = location.hostname;
   if (host === "localhost" || host === "127.0.0.1") return `${location.protocol}//${host}:8789`;
-  if (host.endsWith(".workers.dev") && host.startsWith("easy-poker.")) {
+  // if (host.endsWith(".workers.dev") && host.startsWith("easy-poker.")) {
+  if (host.startsWith("easy-poker.")) {
+
     return `${location.protocol}//${host.replace(/^easy-poker\./, "easy-poker-api.")}`;
   }
   return location.origin;
